276734ce486e0aa40ec3997c2db73e665c5aa402
Hoang Minh Chien <Kydan301193@gmail.com>
2023-03-31 17:09:59 +0700

    Update player,selectedCounterVisual

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -1,16 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
     [SerializeField]private float moveSpeed =7;
     [SerializeField]private GameInput gameInput;
     [SerializeField] private LayerMask countersLayermask;
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;//quay don dep duoc chon
 
+    private void Awake()
+    {
+        Instance = this;
+    }
     private void Start()
     {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
@@ -18,36 +31,23 @@ public class Player : MonoBehaviour
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        if (moveDir != Vector3.zero)
+        if(selectedCounter !=null)
         {
-            lastInteractDir = moveDir;
-        }
-        float interactDistance = 2f;
-
-        if (Physics.Raycast(transform.position, moveDir, out RaycastHit raycastHit, interactDistance, countersLayermask))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                //has clear counter
-                //t??ng ???ng v?i getcomponent bình th??ng nh?ng có thêm if ! null
-                clearCounter.Interact();
-            }
-        }
+            selectedCounter.Interact();
+        }    
+        
     }
 
     void Update()
     {
         HandleMovement();
-        //HandleOInteractions();
+        HandleOInteractions();
     }
     public bool IsWalking()
     {
         return isWalking;
     }    
-    private void HandleOInteractions()//x? lý các t??ng tác
+    private void HandleOInteractions()//xu ly cac tuong tac
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
@@ -63,11 +63,30 @@ public class Player : MonoBehaviour
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 //has clear counter
-                //t??ng ???ng v?i getcomponent bình th??ng nh?ng có thêm if ! null
 
-                clearCounter.Interact();
+                //clearCounter.Interact();
+                if(clearCounter != selectedCounter)
+                {
+                    //selectedCounter = clearCounter;
+                    //OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+                    //{
+                    //    selectedCounter = this.selectedCounter
+                    //    //selectedCounter dau tien la cua class OnSelected
+                    //    //SelectedCounter thu 2 la khai bao ban dau cua class player
+                    //}) ;
+
+                    SetSelectedCounter(clearCounter);
+                }    
+            }  
+            else
+            {
+                SetSelectedCounter(null);
             }    
         }    
+        else
+        {
+            SetSelectedCounter(null);
+        }
     }    
     private void HandleMovement()
     {
@@ -117,5 +136,13 @@ public class Player : MonoBehaviour
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }    
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
+            selectedCounter = this.selectedCounter
+        });
     }    
 }
