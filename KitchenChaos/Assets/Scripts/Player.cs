ebdeae85479bf2efbdf67f320d319904aafbb2db
Hoang Minh Chien <Kydan301193@gmail.com>
2023-03-31 10:14:47 +0700

    collision detection_update script player

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -16,9 +16,46 @@ public class Player : MonoBehaviour
        
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = moveDir != Vector3.zero;
-        this.transform.position += moveDir*moveSpeed*Time.deltaTime;
-        //this.transform.forward = moveDir;
-        //su dung noi suy de cho xoay nhan vat khong qua dot ngot
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.4f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius, moveDir, moveDistance);
+        //nhi?u khi b? dính t??ng không di chuy?n ???c,c?n check thêm
+        if(!canMove)
+        {
+            //cannot move towards modir
+
+            //attemp only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if(canMove)
+            {
+                //Can move only on the X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //cannot move only on the X
+                //attemp only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if(canMove)
+                {
+                    //can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //cannot move in any direction
+                }
+            }
+        }    
+        if(canMove)
+        {
+            this.transform.position += moveDir * moveDistance;
+        }    
+        
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,moveDir, Time.deltaTime *rotateSpeed);
     }
